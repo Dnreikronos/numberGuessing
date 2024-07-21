6c7b844dd0aac82b9599aac90d49d23a1632de14
João Roberto Soares <dnreikronos2@gmail.com>
2024-07-21 10:01:24 -0300

    Did some ajustments in Css

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let attempts = 0;
 
 let myLabel = document.getElementById("myLabel");
 
-
+let myH5 = document.getElementById("myH5");
 let myH3 = document.getElementById("myH3");
 let running ;
 
@@ -16,22 +16,23 @@ submitBtn.onclick =  function() {
     let myNumber = document.getElementById("myNumber").value;
     running = true;
 
-    console.log(correctAnswer);
-
     while(running){
-
+        
         if (myNumber < minNum || myNumber > maxNum) {
             window.alert("Please enter a valid number!");
+            document.reset();
         }
         else{
             attempts ++;
             if(myNumber < correctAnswer){
                 myH3.innerHTML = (`TO LOW! TRY AGAIN`);
                 document.reset();
+                myH5.innerHTML = `Attempts : ${attempts}`
                 
             }
             else if(myNumber > correctAnswer){
                 myH3.innerHTML = (`TO HIGH! TRY AGAIN`);
+                myH5.innerHTML = `Attempts : ${attempts}`
                 document.reset();
             }
             else{
